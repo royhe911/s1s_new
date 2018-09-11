6df64c9722875121cc8805bc7a64b25318834190
roy <123>
2018-09-11 22:58:20 +0800

    database

--- application/database.php
+++ application/database.php
@@ -13,18 +13,15 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => 'sh-cdb-7owrztwe.sql.tencentcdb.com',
-    // 'hostname'       => '127.0.0.1',
+    'hostname'       => '127.0.0.1',
     // 数据库名
     'database'       => 's1s_new',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'Maoke#170310',
-    // 'password'       => '',
+    'password'       => '123456',
     // 端口
-    'hostport'       => '63665',
-    // 'hostport'       => '3306',
+    'hostport'       => '3306',
     // 连接dsn
     'dsn'            => '',
     // 数据库连接参数
